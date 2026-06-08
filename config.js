4905583feb3f025cd6dfd768540e2a1c03463885
smpn1bansei <jayo88@admin.smp.belajar.id>
2026-06-08 23:55:07 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdkf6-EzUHc7AIrwOUz9KdAYlUBmz0QVA",
+    authDomain: "spmb26-a40eb.firebaseapp.com",
+    projectId: "spmb26-a40eb",
+    storageBucket: "spmb26-a40eb.firebasestorage.app",
+    messagingSenderId: "657771871334",
+    appId: "1:657771871334:web:108066841d19d820ae41a7"
+};
+
+// Atur status pendaftaran di sini
+// true = Pendaftaran Terbuka (bisa akses form)
+// false = Pendaftaran Ditutup (user akan dialihkan ke home dan diminta menghubungi admin)
+const STATUS_PENDAFTARAN_DIBUKA = true;
